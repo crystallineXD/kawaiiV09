e74ff2dcb662b67c24307fd6d6e0c432f3b47912
D'anang.W.S™✓ <98865918+Danangws@users.noreply.github.com>
2022-06-30 12:07:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -56,8 +56,8 @@ fs.watchFile(file_exif, () => {
 })
 
 // Sticker WM
-global.packname = sticker_packname
-global.author = sticker_author
+global.packname = 'Kawaii_Botz™✓'
+global.author = 'Danang.w.s™✓'
 global.wm = '© Danang.w.s™✓'
 
 global.multiplier = 1000 // The higher, The harder levelup
